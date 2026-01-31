88418eaa6ccf4ffe133a87417248f35ec396823c
Prajwal Kharode <prajwalkharode18@gmail.com>
2026-01-31 21:23:37 +0530

    introduction to JSX

--- App.js
+++ App.js
@@ -7,6 +7,17 @@ const heading = React.createElement(
     "Hello world from React!!",
 );
 
+// Babel Transpilation JSX to JS
+// JSX => Babel transpiles it to React.createElement => ReactElement which is a JS Object => HTML (DOM)
+const jsxHeading = <h1 id="heading">Hello world from JSX!!</h1>;
+
+// when line breaks jsx should be in ()
+const jsxHeading2 = (
+    <h1 id="heading" className="header">
+        Hello world from JSX 2!!
+    </h1>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(jsxHeading);
