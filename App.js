db3e63664243d4ee513d9d433210262dad96a107
Prajwal Kharode <prajwalkharode18@gmail.com>
2026-02-01 09:57:49 +0530

    assignment ep-03

--- App.js
+++ App.js
@@ -1,30 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const TitleComponent = () => {
-    return <h2>Hello from Functional Component</h2>;
-};
+// Creating Nested Headers using React.createElement() which returns a React element
+const headersStack = React.createElement("div", { id: "title" }, [
+    React.createElement("h1", { key: "h1" }, "This is H1 tag"),
+    React.createElement("h2", { key: "h2" }, "This is H2 tag"),
+    React.createElement("h3", { key: "h3" }, "This is H3 tag"),
+]);
 
-// Component Composition - Nesting of components
-const HeadingComponent = () => {
+// Creating Nested Headers with Functional Component
+const HeaderStack = () => {
     return (
-        <div>
-            {/* different ways to render a component */}
-            <TitleComponent />
-            <TitleComponent></TitleComponent>
-            {TitleComponent()}
-            <h1>This is a Heading Component</h1>
+        <div id="title">
+            <h1>This is H1 tag</h1>
+            <h2>This is H2 tag</h2>
+            <h3>This is H3 tag</h3>
         </div>
     );
 };
 
-// We can put Functional Component inside React Element or vice-versa
-const title = (
-    <div id="title">
-        Hello from React Element
-        <HeadingComponent />
-    </div>
-);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(title);
+// root.render(headersStack);
+root.render(<HeaderStack />);
+
+/********************************************** NOTES ****************************************************************/
+// <HeaderStack />  is a Functional Component (A Function which returns JSX)
+// When you call it with JSX it becomes an React Element
+const element = <HeaderStack />;
+console.log(element); // { type: HeaderStack, props: {}, ... }
