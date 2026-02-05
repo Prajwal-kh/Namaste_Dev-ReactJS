83e18cc9d271e881a6538a1267d839a2314c555b
Prajwal Kharode <prajwalkharode18@gmail.com>
2026-02-05 18:49:29 +0530

    Food delivery app - initial commit

--- App.js
+++ App.js
@@ -1,31 +1,127 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Creating Nested Headers using React.createElement() which returns a React element
-const headersStack = React.createElement("div", { id: "title" }, [
-    React.createElement("h1", { key: "h1" }, "This is H1 tag"),
-    React.createElement("h2", { key: "h2" }, "This is H2 tag"),
-    React.createElement("h3", { key: "h3" }, "This is H3 tag"),
-]);
+// Create a Food Delivery App
+// Design App using Wireframe & plan -> Header, Body, Footer components
+// Header -> Logo, Nav Items, Cart
+// Body -> Search Bar, Restaurant Container which will have Restaurant Cards
+// Restaurant Card -> Image, Name, Cusine, Ratings, Delivery Time
+// Footer -> Copyright, Links, Address, Contact
 
-// Creating Nested Headers with Functional Component
-const HeaderStack = () => {
+const HeaderComponet = () => {
     return (
-        <div id="title">
-            <h1>This is H1 tag</h1>
-            <h2>This is H2 tag</h2>
-            <h3>This is H3 tag</h3>
+        <div className="header">
+            <div className="logo-container">
+                <img
+                    src="https://thumbs.dreamstime.com/b/food-delivery-logo-design-template-134749604.jpg"
+                    alt="Food Delivery App logo"
+                    className="logo"
+                />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
         </div>
     );
 };
 
+const RestroCard = (props) => {
+    const { name, cuisines, ratings, time } = props; // Destructuring props
+    return (
+        <>
+            <div className="restaurant-card">
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtr3m8ja5E320tKLeyGSJWUW6Hj4zOPTvmTs9Vs4S8wQ&s&ec=121528417"
+                    alt="Restaurant 1"
+                />
+                <div className="restaurant-details">
+                    <h3>{name}</h3>
+                    <p>{`Cuisine: ${[...cuisines]}`}</p>
+                    <p>{`Ratings: ${ratings}`}</p>
+                    <p>{`Delivery time: ${time} mins`}</p>
+                </div>
+            </div>
+        </>
+    );
+};
+
+const BodyComponent = () => {
+    return (
+        <div className="body">
+            <div className="search-bar">
+                <input
+                    name="restaurantName"
+                    type="text"
+                    placeholder="Search for restaurants or cuisines"
+                    className="search-input"
+                />
+                <button type="button" className="search-button">
+                    Search
+                </button>
+            </div>
+            <div className="restaurant-container">
+                <RestroCard
+                    name="Restaurant1"
+                    cuisines={["Italian, Mexican"]}
+                    ratings={"4.5"}
+                    time={"40"}
+                />
+                <RestroCard
+                    name="Restaurant2"
+                    cuisines={["Chinese, Thai"]}
+                    ratings={"4.2"}
+                    time={"30"}
+                />
+                <RestroCard
+                    name="Restaurant3"
+                    cuisines={["Indian, Continental"]}
+                    ratings={"4.8"}
+                    time={"50"}
+                />
+                <RestroCard
+                    name="Restaurant4"
+                    cuisines={["Mediterranean, Greek"]}
+                    ratings={"4.3"}
+                    time={"35"}
+                />
+                <RestroCard
+                    name="Restaurant5"
+                    cuisines={["Japanese, Korean"]}
+                    ratings={"4.6"}
+                    time={"45"}
+                />
+                <RestroCard
+                    name="Restaurant6"
+                    cuisines={["French, Spanish"]}
+                    ratings={"4.4"}
+                    time={"55"}
+                />
+            </div>
+        </div>
+    );
+};
+const FooterComponent = () => {
+    return (
+        <div className="footer">
+            <h1>Footer Component</h1>
+        </div>
+    );
+};
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <HeaderComponet />
+            <BodyComponent />
+            <FooterComponent />
+        </div>
+    );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(headersStack);
-root.render(<HeaderStack />);
-
-/********************************************** NOTES ****************************************************************/
-// <HeaderStack />  is a Functional Component (A Function which returns JSX)
-// When you call it with JSX it becomes an React Element
-const element = <HeaderStack />;
-console.log(element); // { type: HeaderStack, props: {}, ... }
+root.render(<AppLayout />);
