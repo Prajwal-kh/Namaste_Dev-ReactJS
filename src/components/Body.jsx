c1720d22ec880554cf09a4697cdff0ca5f388b1c
Prajwal Kharode <prajwalkharode18@gmail.com>
2026-02-08 14:23:58 +0530

    enabled search functionality

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { restaurants } from "../utils/mockData";
 import { RestroCard } from "./RestroCard";
 
 const Body = () => {
     const [listOfRestaurants, setListOfRestaurants] =
         React.useState(restaurants);
+    const [restaurantName, setRestaurantName] = React.useState("");
     // const handleFilter = () => {
     //     const filteredList = listOfRestaurants.filter(
     //         (restaurant) => restaurant.info.avgRating >= 4.0,
     //     );
     //     setListOfRestaurants(filteredList);
     // };
+    useEffect(() => {
+        const filteredList = restaurants.filter((restaurant) =>
+            restaurant.info.name
+                .toLowerCase()
+                .includes(restaurantName.toLowerCase()),
+        );
+        setListOfRestaurants(filteredList);
+    }, [restaurantName, setRestaurantName]);
+
     return (
         <div className="body">
             <div className="search-bar">
@@ -19,6 +29,10 @@ const Body = () => {
                     type="text"
                     placeholder="Search for restaurants or cuisines"
                     className="search-input"
+                    value={restaurantName}
+                    onChange={(e) => {
+                        setRestaurantName(e.target.value);
+                    }}
                 />
                 <button type="button" className="search-button">
                     Search
