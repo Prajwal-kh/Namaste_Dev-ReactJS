fcabaad150b9e840867c225a14f50e567b23628b
Prajwal Kharode <prajwalkharode18@gmail.com>
2026-02-11 21:19:00 +0530

    added basic login btn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/config";
 
 const Header = () => {
+    const [userStatus, setUserStatus] = useState("Login");
     return (
         <div className="header">
             <div className="logo-container">
@@ -16,6 +18,19 @@ const Header = () => {
                     <li>About</li>
                     <li>Contact</li>
                     <li>Cart</li>
+                    <li>
+                        <button
+                            type="button"
+                            className="logout-btn"
+                            onClick={() =>
+                                setUserStatus(
+                                    userStatus === "Login" ? "Logout" : "Login",
+                                )
+                            }
+                        >
+                            {userStatus}
+                        </button>
+                    </li>
                 </ul>
             </div>
         </div>
